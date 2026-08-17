0be4d3e5106e9ca1184a3226cf4dc75c104a68f0
MariaPimenta <dudamepimenta@gmail.com>
2026-08-17 15:57:46 -0300

    Configuração do ambiente virtual e bibliotecas base

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Projeto iniciado!")
+import numpy as np
+import pandas as pd
+import scipy
+import matplotlib
 
-
-if __name__ == "__main__":
-    main()
+print("🚀 Ambiente virtual configurado e isolado com sucesso!")
+print(f"Versão do NumPy: {np.__version__}")
+print(f"Versão do Pandas: {pd.__version__}")
+print(f"Versão do SciPy: {scipy.__version__}")
+print(f"Versão do Matplotlib: {matplotlib.__version__}")
